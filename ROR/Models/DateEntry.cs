e89e4de6f07b64459118c14ce7f7a091c0146751
Geert Haans <geert.haans@gmail.com>
2025-02-19 11:24:48 +0100

    feat: add summaries to DateEntry [CX-37]

--- ROR/Models/DateEntry.cs
+++ ROR/Models/DateEntry.cs
@@ -2,16 +2,29 @@ using System.Text.Json.Serialization;
 
 namespace ROR.Net.Models;
 
+/// <summary>
+/// Represents a date entry in the ROR database.
+/// </summary>
 public class DateEntry
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DateEntry"/> class.
+    /// </summary>
+    /// <param name="schemaVersion">The schema version of the date entry.</param>
     public DateEntry(string schemaVersion)
     {
         SchemaVersion = schemaVersion;
     }
-
+    
+    /// <summary>
+    /// Gets or sets the date of the entry.
+    /// </summary>
     [JsonPropertyName("date")]
     public required string Date { get; set; }
 
+    /// <summary>
+    /// Gets or sets the schema version of the date entry.
+    /// </summary>
     [JsonPropertyName("schema_version")]
     public string SchemaVersion { get; set; }
 }
