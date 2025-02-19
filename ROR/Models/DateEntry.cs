2648019ed42562fd558a85d1b91946822b7a0f6b
Ben Stokmans <benstokmans@hotmail.nl>
2025-02-19 16:39:08 +0100

    test: implement test for all classes with logic

--- ROR/Models/DateEntry.cs
+++ ROR/Models/DateEntry.cs
@@ -4,11 +4,6 @@ namespace ROR.Net.Models;
 
 public class DateEntry
 {
-    public DateEntry(string schemaVersion)
-    {
-        SchemaVersion = schemaVersion;
-    }
-
     [JsonPropertyName("date")]
     public required string Date { get; set; }
 
