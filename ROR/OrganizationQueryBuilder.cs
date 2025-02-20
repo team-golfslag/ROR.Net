11ad537cc62753ec3da88087e8edb9d52ca53d4e
Ben Stokmans <benstokmans@hotmail.nl>
2025-02-20 13:44:24 +0100

    revert: merge conflict artifact

--- ROR/OrganizationQueryBuilder.cs
+++ ROR/OrganizationQueryBuilder.cs
@@ -243,9 +243,9 @@ public class OrganizationQueryBuilder
 
         // Combine the results
         OrganizationsResult first = results[0];
-        return results.Count == 1
-            ? first
-            : results.Skip(1).Aggregate(first, (current, other) => current.Combine(other));
+        if (results.Count == 1) return first;
+
+        return results.Skip(1).Aggregate(first, (current, other) => current.Combine(other));
     }
 
     /// <summary>
