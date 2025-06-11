1ef611976edb19766a2d0aed273ca320d4ad73d8
Ben Stokmans <benstokmans@hotmail.nl>
2025-06-11 22:16:03 +0200

    fix: base url issue and deprecated models

--- ROR.Net/Models/Organization.cs
+++ ROR.Net/Models/Organization.cs
@@ -14,13 +14,7 @@ public class Organization
     /// </summary>
     [JsonPropertyName("id")]
     public required string Id { get; set; }
-
-    /// <summary>
-    /// A field with information about the ROR record, including dates when the record was created and last modified.
-    /// </summary>
-    [JsonPropertyName("admin")]
-    public required OrganizationAdmin Admin { get; set; }
-
+    
     /// <summary>
     /// The domains registered to a particular institution, not including the protocol, path portions,
     /// or query parameters that may exist in the URL for the organization's website.
@@ -34,15 +28,7 @@ public class Organization
     /// </summary>
     [JsonPropertyName("established")]
     public int? Established { get; set; }
-
-    /// <summary>
-    /// Other identifiers for the organization (if available).
-    /// ROR maps its IDs to four types of external identifiers:
-    /// GRID, Wikidata, ISNI, and the Crossref Open Funder Registry (formerly “FundRef”).
-    /// </summary>
-    [JsonPropertyName("external_ids")]
-    public List<OrganizationExternalId>? ExternalIds { get; set; }
-
+    
     /// <summary>
     /// The primary website and Wikipedia page for the organization.
     /// Only one website URL and one Wikipedia URL should be associated with the record.
@@ -53,23 +39,8 @@ public class Organization
     /// Otherwise, the English version will be used.
     /// </remarks>
     [JsonPropertyName("links")]
-    public List<OrganizationLink>? Links { get; set; }
-
-    /// <summary>
-    /// The location(s) of the organization, including continent name, continent code, country name,
-    /// two-letter ISO-3166 country code, country subdivision name (e.g., Canadian province, Japanese prefecture or US state),
-    /// country subdivision code, latitude, longitude, and specified location (usually city) name.
-    /// Location data comes from GeoNames.
-    /// </summary>
-    [JsonPropertyName("locations")]
-    public required List<OrganizationLocation> Locations { get; set; }
-
-    /// <summary>
-    /// Names for the organization, including four types of names: acronyms, aliases, labels, and the name.
-    /// </summary>
-    [JsonPropertyName("names")]
-    public required List<OrganizationName> Names { get; set; }
-
+    public List<string>? Links { get; set; }
+    
     /// <summary>
     /// One or more organizations in ROR that the organization is related to.
     /// </summary>
